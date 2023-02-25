fe7d678e643f6b56d2b6cf17ead8aaec601555b7
anmol <anmolpokhrel46@gmail.com>
2023-02-25 20:19:58 +0530

    fetch exchange rate

--- src/components/SearchModal/index.tsx
+++ src/components/SearchModal/index.tsx
@@ -1,51 +1,60 @@
-import React, { useState } from "react"
-import Input from "../elements/Input"
-import TokenItem from "../elements/TokenItem"
-import { tokenData } from "../../constants/mockdata"
-import { TokenData } from "../SwapWidget/types"
+import React, { useState } from "react";
+import Input from "../elements/Input";
+import TokenItem from "../elements/TokenItem";
+import { tokenData } from "../../constants/mockdata";
+import { TokenData } from "../SwapWidget/types";
 type SearchModal = {
-  type: "from" | "to"
-}
+    type: "from" | "to";
+};
 
 const SearchModal = (props: SearchModal) => {
-  const [tokens, setTokens] = useState(tokenData.tokens.slice(0, 10))
-  const [searchWord, setSearchWord] = useState<string>("")
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    setSearchWord(e.target.value)
-    if (e.target.value.trim()) {
-      setTokens(
-        tokenData.tokens
-          .filter((token) => {
-            return token.name.toLowerCase().includes(e.target.value.toLowerCase())
-          })
-          .slice(0, 100)
-      )
-    }
-  }
-  return (
-    <div>
-      <Input
-        type="text"
-        placeholder="Search"
-        id="price"
-        onChange={handleChange}
-        value={searchWord}
-        className="my-1 px-4 py-1 text-xl border border-gray-600 text-gray-400 outline-none font-mono w-full rounded-lg bg-transparent"
-      />
-      <ul className="w-full list-none h-full overflow-y-scroll overflow-x-hidden scrollbar-hide">
-        {tokens.map((token, index) => (
-          <TokenItem
-            key={index}
-            tokenName={token.name ? token.name : token.symbol ? token.symbol : "Unknown"}
-            tokenImage={token.logoURI || ""}
-            tokenSymbol={token.symbol}
-            type={props.type}
-          />
-        ))}
-      </ul>
-    </div>
-  )
-}
+    const [tokens, setTokens] = useState(tokenData.tokens.slice(0, 10));
+    const [searchWord, setSearchWord] = useState<string>("");
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setSearchWord(e.target.value);
+        if (e.target.value.trim()) {
+            setTokens(
+                tokenData.tokens
+                    .filter((token) => {
+                        return token.name
+                            .toLowerCase()
+                            .includes(e.target.value.toLowerCase());
+                    })
+                    .slice(0, 100)
+            );
+        }
+    };
+    return (
+        <div>
+            <Input
+                type="text"
+                placeholder="Search"
+                id="price"
+                onChange={handleChange}
+                value={searchWord}
+                className="my-1 px-4 py-1 text-xl border border-gray-600 text-gray-400 outline-none font-mono w-full rounded-lg bg-transparent"
+            />
+            <ul className="w-full list-none h-full overflow-y-scroll overflow-x-hidden scrollbar-hide">
+                {tokens.map((token, index) => (
+                    <TokenItem
+                        key={index}
+                        tokenName={
+                            token.name
+                                ? token.name
+                                : token.symbol
+                                ? token.symbol
+                                : "Unknown"
+                        }
+                        tokenImage={token.logoURI || ""}
+                        tokenSymbol={token.symbol}
+                        type={props.type}
+                        tokenAddress={token.address}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
 
-export default SearchModal
+export default SearchModal;
--- src/components/SwapWidget/WidgetProvider.tsx
+++ src/components/SwapWidget/WidgetProvider.tsx
@@ -1,64 +1,108 @@
-import React, { useState, createContext, useContext } from "react"
-import { TokenData } from "./types"
-
+import React, { useState, createContext, useContext, useEffect } from "react";
+import { TokenData } from "./types";
+import { tokenData } from "../../constants/mockdata";
+import { parseEther } from "ethers";
 export interface WidgetContextProps {
-  showWidgetModal: boolean
-  showSearchModal: boolean
-  toToken: TokenData
-  fromToken: TokenData
-  toggleWidget: () => void
-  toggleSearch: () => void
-  setToToken: (token: TokenData) => void
-  setFromToken: (token: TokenData) => void
+    showWidgetModal: boolean;
+    showSearchModal: boolean;
+    toToken: TokenData;
+    fromToken: TokenData;
+    toggleWidget: () => void;
+    toggleSearch: () => void;
+    setToToken: (token: TokenData) => void;
+    setFromToken: (token: TokenData) => void;
 }
 
 export const WidgetContext = createContext<WidgetContextProps>({
-  showSearchModal: false,
-  showWidgetModal: false,
-  toToken: {},
-  fromToken: {},
-  toggleWidget: () => {},
-  toggleSearch: () => {},
-  setToToken: (token: TokenData) => {},
-  setFromToken: (token: TokenData) => {},
-})
+    showSearchModal: false,
+    showWidgetModal: false,
+    toToken: {
+        ...(tokenData.tokens[1] as unknown as TokenData),
+    },
+    fromToken: {
+        ...(tokenData.tokens[0] as unknown as TokenData),
+    },
+    toggleWidget: () => {},
+    toggleSearch: () => {},
+    setToToken: (token: TokenData) => {},
+    setFromToken: (token: TokenData) => {},
+});
 
 export const useWidgetContext = () => {
-  return useContext(WidgetContext)
-}
+    return useContext(WidgetContext);
+};
 
 const WidgetProvider = ({ children }: { children: React.ReactNode }) => {
-  const toggleWidget = () => {
-    setWidgetData((prev) => ({ ...prev, showWidgetModal: !prev.showWidgetModal }))
-  }
+    const toggleWidget = () => {
+        setWidgetData((prev) => ({
+            ...prev,
+            showWidgetModal: !prev.showWidgetModal,
+        }));
+    };
 
-  const toggleSearch = () => {
-    setWidgetData((prev) => ({ ...prev, showSearchModal: !prev.showSearchModal }))
-  }
+    const toggleSearch = () => {
+        setWidgetData((prev) => ({
+            ...prev,
+            showSearchModal: !prev.showSearchModal,
+        }));
+    };
 
-  const setToToken = (token: TokenData) => {
-    setWidgetData((prev) => ({ ...prev, toToken: token }))
-  }
+    const setToToken = (token: TokenData) => {
+        setWidgetData((prev) => ({
+            ...prev,
+            toToken: { ...token },
+        }));
+    };
 
-  const setFromToken = (token: TokenData) => {
-    setWidgetData((prev) => ({ ...prev, fromToken: token }))
-  }
+    const setFromToken = (token: TokenData) => {
+        setWidgetData((prev) => ({
+            ...prev,
+            fromToken: { ...token },
+        }));
+    };
 
-  const [widgetData, setWidgetData] = useState({
-    showWidgetModal: false,
+    const [widgetData, setWidgetData] = useState({
+        showWidgetModal: false,
+        showSearchModal: false,
+        toToken: {
+            ...(tokenData.tokens[1] as unknown as TokenData),
+        },
+        fromToken: {
+            ...(tokenData.tokens[0] as unknown as TokenData),
+        },
+        toggleWidget,
+        toggleSearch,
+        setToToken,
 
-    showSearchModal: false,
-    toToken: {},
-    fromToken: {},
-    toggleWidget,
-    toggleSearch,
-    setToToken,
-    setFromToken,
-  })
+        setFromToken,
+    });
+    useEffect(() => {
+        if (widgetData.fromToken.amount) {
+            console.log(widgetData.fromToken);
+            (async () => {
+                const res = await fetch(
+                    `https://api.1inch.io/v5.0/1/quote?fromTokenAddress=${
+                        widgetData.fromToken?.address
+                    }&toTokenAddress=${
+                        widgetData.toToken?.address
+                    }&amount=${parseEther(
+                        widgetData.fromToken?.amount!.toString()
+                    )}`
+                );
+                // const res = await fetch(
+                //     `https://api.1inch.io/v5.0/1/quote?fromTokenAddress=0x111111111117dC0aa78b770fA6A738034120C302&toTokenAddress=0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9&amount=10000000000000000000`
+                // );
+                const data = await res.json();
+                console.log(data);
+            })();
+        }
+    }, [widgetData.fromToken.address, widgetData.fromToken.amount]);
 
-  console.log("widgetData", widgetData)
+    return (
+        <WidgetContext.Provider value={widgetData}>
+            {children}
+        </WidgetContext.Provider>
+    );
+};
 
-  return <WidgetContext.Provider value={widgetData}>{children}</WidgetContext.Provider>
-}
-
-export default WidgetProvider
+export default WidgetProvider;
